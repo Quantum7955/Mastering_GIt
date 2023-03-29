9e8ea8c77c276165586d53c0a64e66a990d73fb1
Luis Melendez MacStation <cogane7955@MacStation.com>
2023-03-29 08:26:38 -0600

    remove mainexe and add lineas to src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@ int main()
    printf("Hello, world!"); 
    printf("Incredible contribution from lenovo");
    printf("Alice is doing want she was asked to"); 
+   printf("Change add to stagging area");
+    printf("Change add NOT added to stagging area");
    return 0; 
 }
 
